b2c9779763b935a44d47f83de1c90b1ce620aadf
AmeenM <AmeenM@Ameen>
2015-07-08 23:04:59 +0530

    Initial Version

---
+++ MyFirstGITDemo/src/FirstMainClass.java
@@ -0,0 +1,19 @@
+
+public class FirstMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I am in main method");
+		
+		addmethod();
+	}
+
+	private static void addmethod() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I am in static method");
+		
+	}
+
+}
